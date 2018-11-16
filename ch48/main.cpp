1dadfc06f50c29501a2878c3288416d58b2c6758
Mikhail Kochegarov <nexo.michael@gmail.com>
2018-11-16 23:23:14 +0100

    ch48

--- ch48/main.cpp
+++ ch48/main.cpp
@@ -1,22 +1,53 @@
 #include <iostream>
 #include <utility>
 
+enum class MonsterType
+{
+    OGRE,
+    DRAGON,
+    ORC,
+    GIANT,
+    SLIME,
+};
+
+std::string mosterTypeToString(MonsterType t)
+{
+    switch (t)
+    {
+    case MonsterType::OGRE:
+        return "Ogre";
+    case MonsterType::DRAGON:
+        return "Dragon";
+    case MonsterType::ORC:
+        return "Orc";
+    case MonsterType::GIANT:
+        return "Giant";
+    case MonsterType::SLIME:
+        return "Slime";
+    }
+    return "Unknown";
+}
+
 struct Monster
 {
-    std::string type;
+    MonsterType type;
     std::string name;
     int health;
 };
 
 void printMonster(Monster m)
 {
-    std::cout << "This " << m.type << " is named " << m.name << " and has " << m.health << " helath." << std::endl;
+    std::cout
+        << "This " << mosterTypeToString(m.type)
+        << " is named " << m.name
+        << " and has " << m.health
+        << " helath." << std::endl;
 }
 
 int main()
 {
-    Monster torg = {"Ogre", "Torg", 145};
-    Monster blurp = {"Slime", "Blurp", 23};
+    Monster torg = {MonsterType::OGRE, "Torg", 145};
+    Monster blurp = {MonsterType::SLIME, "Blurp", 23};
 
     printMonster(torg);
     printMonster(blurp);
